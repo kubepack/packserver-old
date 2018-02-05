82589550d718028b7e4c1995e4483cdfb5dc472a
Abu Hanifa <a8uhnf@users.noreply.github.com>
2018-02-06 01:50:43 +0600

    Log-audit (#1)
    * parse git-commit-hash from event
    
    * Store db in leveldb
    
    * Pretty JSON, clean up
    
    * Adding docs
    
    * Add docs
    
    * Update doc

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"net/http"
 	"html"
 	"io/ioutil"
@@ -11,16 +12,23 @@ import (
 	"path/filepath"
 	"encoding/json"
 	"k8s.io/apiserver/pkg/apis/audit/v1beta1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/kubernetes/pkg/kubectl/scheme"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
 	AppName = "log-audit"
 )
+var mux sync.Mutex
 
 func main() {
 	fmt.Println("Server Started...")
+
+	// Test Code @TODO: Hanif
+	err := os.RemoveAll(filepath.Join(os.TempDir(), AppName))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Test Code Ends
 	routine := 0
 	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/events" {
@@ -33,14 +41,13 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Println(string(resp))
 		eventList := &v1beta1.EventList{}
 		err = json.Unmarshal(resp, eventList)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		routine += 1
-		go OpenLevelDB(eventList, routine)
+		go ProcessEvents(eventList, routine)
 		if err != nil {
 			fmt.Println(err)
 			log.Fatalln(err)
@@ -52,66 +59,146 @@ func main() {
 			return
 		}
 		fmt.Println("hello request222")
-		fmt.Fprintf(writer, "Hello %q", html.EscapeString(request.URL.Path))
-	})
+		writer.Header().Set("Content-Type", "application/json")
 
+		resp := map[string]v1beta1.EventList{}
+		db, err := OpenGoLevelDB()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer db.Close()
+
+		iter := db.NewIterator(nil, nil)
+
+		for iter.Next() {
+			// Remember that the contents of the returned slice should not be modified, and
+			// only valid until the next call to Next.
+			key := iter.Key()
+			value := iter.Value()
+			tmpEventList := &v1beta1.EventList{}
+			err = json.Unmarshal(value, tmpEventList)
+			resp[string(key)] = *tmpEventList
+			if err != nil {
+				fmt.Println("Error to Unmarshal")
+			}
+		}
+
+
+		iter.Release()
+		err = iter.Error()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		res, err := json.Marshal(resp)
+		if err != nil {
+			fmt.Println(err)
+		}
+		writer.Write(res)
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func OpenLevelDB(list *v1beta1.EventList, routine int) error {
-	fmt.Printf("Routine Number %d\n", routine)
-	fmt.Printf("Hello %s\n", list.Kind)
+func ProcessEvents(list *v1beta1.EventList, routine int) error {
 	if list == nil {
-		 fmt.Println("Nil")
-		 return fmt.Errorf("%s", "Empty event list")
-	}
-	for _, val := range list.Items {
-		fmt.Println("-----------------------")
-		fmt.Println(routine)
-		_, err := json.MarshalIndent(val, "", "  ")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		// fmt.Println(string(mar))
-		fmt.Println(val.ObjectRef)
-		// fmt.Println(val.ObjectRef.Name)
-		// fmt.Println(val.ObjectRef.Namespace)
-		if val.ResponseObject != nil {
-			fmt.Println("********************")
-			var ro runtime.TypeMeta
-			if err := json.Unmarshal(val.ResponseObject.Raw, &ro); err != nil {
-				return err
-			}
-			kind := ro.GetObjectKind().GroupVersionKind()
-			versionedObject, err := scheme.Scheme.New(kind)
-			err = json.Unmarshal(val.ResponseObject.Raw, versionedObject)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			fmt.Println("---", versionedObject)
-			// obj := versionedObject.DeepCopyObject()
-			// fmt.Println(obj)
-
-			fmt.Println(string(val.ResponseObject.Raw))
-		}
+		fmt.Println("Nil")
+		return fmt.Errorf("%s", "Empty event list")
 	}
 
-	path := filepath.Join(os.TempDir(), AppName)
-
-	if _, err := os.Stat(path); err != nil {
-		err := os.Mkdir(path, 0755)
-		if err != nil {
-			return err
-		}
-	}
-	db, err := leveldb.OpenFile(path, nil)
+	db, err := OpenGoLevelDB()
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
+	mapToGitCommitHash := map[string]*v1beta1.EventList{}
+	eventList := &v1beta1.EventList{}
+	var events []*v1beta1.Event
+	for _, val := range list.Items {
+		fmt.Println(routine)
+		_, err := json.MarshalIndent(val, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if val.ResponseObject != nil {
+			type Item struct {
+				metav1.TypeMeta   `json:",inline"`
+				metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+			}
+
+			item := &Item{}
+
+			err := json.Unmarshal(val.ResponseObject.Raw, item)
+			if err != nil {
+				return err
+			}
+
+			gitCommitHash, ok := item.Annotations["git-commit-hash"]
+			if ok {
+				evByte, err := json.Marshal(val)
+				if err != nil {
+					fmt.Println("Error during marshalling event")
+				}
+
+				eventTmp := &v1beta1.Event{}
+				err = json.Unmarshal(evByte, eventTmp)
+				if err != nil {
+					fmt.Println("Error during Unmarshall event byte")
+				}
+				events = append(events, eventTmp)
+				ret, err := db.Has([]byte(gitCommitHash), nil)
+				if !ret {
+					mapToGitCommitHash[gitCommitHash] = &v1beta1.EventList{}
+				} else {
+					dbEventList, err := db.Get([]byte(gitCommitHash), nil)
+					if err != nil {
+						fmt.Println("Error to get from db...")
+					}
+
+					tmpEventList := &v1beta1.EventList{}
+					err = json.Unmarshal(dbEventList, tmpEventList)
+					if err != nil {
+						fmt.Println("Error to Unmarshal...", err)
+					}
+					mapToGitCommitHash[gitCommitHash] = tmpEventList
+				}
+				eventList = mapToGitCommitHash[gitCommitHash]
+				eventList.Items = append(eventList.Items, *eventTmp)
+				mapToGitCommitHash[gitCommitHash] = eventList
+			}
+		}
+	}
+	mux.Lock()
+	defer mux.Unlock()
+	for key, value := range mapToGitCommitHash {
+		tmpEventListByte, err := json.Marshal(value)
+		fmt.Println(string(tmpEventListByte))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = db.Put([]byte(key), tmpEventListByte, nil)
+		if err != nil {
+			fmt.Println("Error to PUT into DB")
+		}
+
+	}
 
 	return nil
+}
+
+func OpenGoLevelDB() (*leveldb.DB, error) {
+	path := filepath.Join(os.TempDir(), AppName)
+
+	if _, err := os.Stat(path); err != nil {
+		err := os.Mkdir(path, 0755)
+		if err != nil {
+			return nil, err
+		}
+	}
+	db, err := leveldb.OpenFile(path, nil)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
